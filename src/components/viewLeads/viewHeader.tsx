e3c77b19e9752fc643a8e0fb98407c6bae2daf86
Harjeevan Tanda <harjeevan.tanda@gmail.com>
2022-12-17 01:58:11 -0500

    update view lead details page

--- src/components/viewLeads/viewHeader.tsx
+++ src/components/viewLeads/viewHeader.tsx
@@ -1,41 +1,38 @@
+import React from "react";
+import Router from "next/router";
 import { Box, Grid, Button, Typography } from "@mui/material";
 import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
 import { useForm } from "react-hook-form";
 import type { SubmitHandler } from "react-hook-form";
 import FormInputDropdown from "../shared/formInputs/FormInputDropdown";
-import React from "react";
 import { status } from "../../constants/constant";
 const ViewHeader = () => {
-  console.log(status);
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-    formState,
-  } = useForm<any>({
+  const { handleSubmit, control } = useForm<any>({
     mode: "onChange",
   });
   const onSubmit: SubmitHandler<any> = async (data, e: any) => {};
   return (
     <div>
       <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Button variant="contained" onClick={() => Router.back()}><ArrowCircleLeftIcon className="mr-2" />Back</Button>
+        <Button variant="contained" onClick={() => Router.back()}>
+          <ArrowCircleLeftIcon className="mr-2" />
+          Back
+        </Button>
         <Typography variant="h6">{"View Leads"}</Typography>
         <Box className="">
           <form onSubmit={handleSubmit(onSubmit)}>
-            <Grid container spacing={3}>
-              <Grid className="w-44">
+            <Grid container>
+              <Grid sx={{ minWidth: 100 }}>
                 <FormInputDropdown
                   name={"status"}
                   control={control}
                   label={"Status"}
                   data={status}
                   defaultValue=""
-                  errors={errors}
                 />
               </Grid>
               <Grid>
-                <Button className="m" size="small" variant="contained" type="submit">
+                <Button size="small" variant="contained" type="submit">
                   {"Search"}
                 </Button>
               </Grid>
